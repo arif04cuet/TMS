b56443c38d13d1d59a001358ac35fe53c742960c
Shahid <shahidcse6@gmail.com>
2020-04-14 13:05:35 +0600

    return errors in development mode

--- Modules/Core/Module.Core/Filters/ExceptionFilter.cs
+++ Modules/Core/Module.Core/Filters/ExceptionFilter.cs
@@ -1,13 +1,24 @@
 ﻿using Infrastructure;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
 
 namespace Module.Core.Filters
 {
     public class ExceptionFilter : IExceptionFilter
     {
+        private readonly IWebHostEnvironment _env;
+        public ExceptionFilter(IWebHostEnvironment env)
+        {
+            _env = env;
+        }
+
         public void OnException(ExceptionContext context)
         {
+            System.Console.WriteLine(context.Exception.Message);
             if (context.Exception is BusinessExceptionBase)
             {
                 var exception = (BusinessExceptionBase)context.Exception;
@@ -23,15 +34,54 @@ namespace Module.Core.Filters
             }
             else
             {
-                context.Result = new ObjectResult(new
+                ObjectResult result;
+                if (_env.IsProduction())
                 {
-                    Status = 500,
-                    Message = "Something went wrong."
-                })
+                    result = GetProductionErrorResult();
+                }
+                else
                 {
-                    StatusCode = 500
-                };
+                    result = GetDevelopmentErrorResult(context);
+                }
+                context.Result = result;
             }
+        }
+
+        private ObjectResult GetDevelopmentErrorResult(ExceptionContext context)
+        {
+            return new ObjectResult(new
+            {
+                Status = 500,
+                Message = "Something went wrong.",
+                Error = CollectErrors(context)
+            })
+            {
+                StatusCode = 500
+            };
+        }
+
+        private ObjectResult GetProductionErrorResult()
+        {
+            return new ObjectResult(new
+            {
+                Status = 500,
+                Message = "Something went wrong."
+            })
+            {
+                StatusCode = 500
+            };
+        }
+
+        private List<string> CollectErrors(ExceptionContext context)
+        {
+            List<string> errors = new List<string>();
+            Exception ex = context.Exception;
+            while (ex != null)
+            {
+                errors.Add(ex.Message);
+                ex = ex.InnerException;
+            }
+            return errors;
         }
     }
 }
