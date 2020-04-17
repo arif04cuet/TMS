54ecf10bcd9a87f5064d5eb30710fe5d1562f2fd
Shahid <shahidcse6@gmail.com>
2020-04-17 23:32:26 +0600

    enable migrate

--- Infrastructure.Data.EFCore/ServiceRegistrar.cs
+++ Infrastructure.Data.EFCore/ServiceRegistrar.cs
@@ -1,6 +1,9 @@
 ﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using System;
 
 namespace Infrastructure.Data.EFCore
 {
@@ -15,6 +18,7 @@ namespace Infrastructure.Data.EFCore
         {
             var provider = services.BuildServiceProvider();
             var config = provider.GetService<IConfiguration>();
+            var logger = provider.GetService<ILoggerFactory>().CreateLogger<ServiceRegistrar>();
             services.Configure<DataContextOptions>(options =>
             {
                 options.ConnectionString = config.GetConnectionString("Default");
@@ -26,6 +30,18 @@ namespace Infrastructure.Data.EFCore
                 var _dataContext = serviceProvider.GetService<IDataContext>();
                 return ActivatorUtilities.CreateInstance(serviceProvider, typeof(UnitOfWork), _dataContext);
             });
+
+            try
+            {
+                var p = services.BuildServiceProvider();
+                var dataContext = p.GetService<IDataContext>();
+                (dataContext as DbContext)?.Database.Migrate();
+            }
+            catch (Exception e)
+            {
+                logger.LogError($"---MIGRATE ERROR--- at {DateTime.Now}");
+                logger.LogError(e.Message);
+            }
         }
     }
 }
