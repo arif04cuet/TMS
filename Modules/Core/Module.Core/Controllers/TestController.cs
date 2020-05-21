681b792ccbedb2ab063d7c09a9ce9f38338824e3
Shahid <shahidcse6@gmail.com>
2020-05-21 16:52:07 +0600

    server test

---
+++ Modules/Core/Module.Core/Controllers/TestController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace Module.Core.Controllers
+{
+    [ApiController]
+    public class TestController : ControllerBase
+    {
+        private readonly IHttpContextAccessor _httpContextAccessor;
+        private readonly HttpRequest _request;
+
+        public TestController(IHttpContextAccessor httpContextAccessor)
+        {
+            _httpContextAccessor = httpContextAccessor;
+        }
+
+
+        [HttpGet("server")]
+        public ActionResult ServerTest()
+        {
+            var http = _httpContextAccessor.HttpContext;
+            var info = new
+            {
+                Request = new
+                {
+                    Path = http.Request.Path,
+                    Host = new
+                    {
+                        Port = http.Request.Host.Port,
+                        Value = http.Request.Host.Value,
+                        Host = http.Request.Host.Host,
+                        UriComponent = http.Request.Host.ToUriComponent()
+                    },
+                    Scheme = http.Request.Scheme,
+                    Protocol = http.Request.Protocol,
+                    ContentType = http.Request.ContentType
+                },
+                TraceIdentifier = http.TraceIdentifier,
+                Connection = new {
+                    RemoteIpAddress = http.Connection.RemoteIpAddress.ToString(),
+                    RemotePort = http.Connection.RemotePort,
+                    LocalIpAddress = http.Connection.LocalIpAddress.ToString(),
+                    LocalPort = http.Connection.LocalPort
+                }
+            };
+            return Ok(info);
+        }
+
+    }
+}
